f253e632906d1492b1b10a452c5ece9c36ca4b75
David Yell <dyell@ukwebmedia.com>
2018-05-09 11:37:35 +0100

    Added html and also a class

--- docs-tutorial/src/greeter.ts
+++ docs-tutorial/src/greeter.ts
@@ -1,3 +1,10 @@
+class Student {
+    fullName: string;
+    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
 interface Person {
     firstName: string;
     lastName: string;
@@ -7,9 +14,6 @@ function greeter(person: Person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
 
-let user = {
-    firstName: 'Jane',
-    lastName: 'Doe'
-};
+let user = new Student('David', 'S', 'Yell');
 
 document.body.innerHTML = greeter(user);
